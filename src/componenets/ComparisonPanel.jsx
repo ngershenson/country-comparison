c9de46de80c01a193d5e4988a928f68b52c9f7ac
Noah Gershenson <noah.gershenson@powerhrg.com>
2025-10-23 00:29:40 -0400

    Add spacing between country cards
    used nvim btw

--- src/componenets/ComparisonPanel.jsx
+++ src/componenets/ComparisonPanel.jsx
@@ -3,17 +3,19 @@ import CountryCard from './CountryCard'
 
 export default function ComparisonPanel({ countryData, selectedCountryIds, handleRemoveCountry }) {
 
-    return (
-        <div className="comparison-panel">
-            <h2>Selected Countries are</h2>
-            {selectedCountryIds.length === 0
-                && <p>Please Select Countries to View Comparison</p>}
-            {selectedCountryIds.map(id => (
+  return (
+    <div className="comparison-panel">
+      <h2>Selected Countries are</h2>
+      <div className='country-card-container'>
+        {selectedCountryIds.length === 0
+          && <p>Please Select Countries to View Comparison</p>}
+        {selectedCountryIds.map(id => (
 
-                <div key={id}>
-                    <CountryCard data={countryData.filter(c => c.cca3 === id)[0]} handleRemoveCountry={handleRemoveCountry} />
-                </div>
-            ))}
-        </div>
-    )
+          <div key={id}>
+            <CountryCard data={countryData.filter(c => c.cca3 === id)[0]} handleRemoveCountry={handleRemoveCountry} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
